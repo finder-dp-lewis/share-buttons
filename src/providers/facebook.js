4816e19dc0caf146d200e637ff693f56b487c027
David Lewis <david.lewis@hiveempire.com>
2015-04-15 12:43:41 +1000

    Initial commit

---
+++ src/providers/facebook.js
@@ -0,0 +1,19 @@
+/*global Sharebuttons*/
+Sharebuttons.prototype.addProvider({
+  'id': 'facebook',
+
+  'neededBy': function (button) {
+    var returnVal = false, hrefData = Sharebuttons.prototype.parseHref(button);
+    if (hrefData.hostname.indexOf(this.id) !== -1) {
+      returnVal = true;
+    }
+    return returnVal;
+  },
+  fetchShareCount: function (button, callback) {
+    Sharebuttons.prototype.jsonp('https://graph.facebook.com', {
+      id: decodeURIComponent(Sharebuttons.prototype.parseHref(button).parameters.u)
+    }, function (result) {
+      callback(result.shares);
+    });
+  }
+});
---
+++ src/providers/twitter.js
@@ -0,0 +1,22 @@
+/*global Sharebuttons*/
+Sharebuttons.prototype.addProvider({
+  id: 'twitter',
+
+  neededBy: function (button) {
+    var returnVal = false;
+
+    if (Sharebuttons.prototype.parseHref(button).hostname.indexOf(this.id) !== -1) {
+      returnVal = true;
+    }
+
+    return returnVal;
+  },
+
+  fetchShareCount: function (button, callback) {
+    Sharebuttons.prototype.jsonp('https://cdn.api.twitter.com/1/urls/count.json', {
+      url: decodeURIComponent(Sharebuttons.prototype.parseHref(button).parameters.url)
+    }, function (result) {
+      callback(result.count);
+    });
+  }
+});
